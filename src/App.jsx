2b51a25ab3d4ecd07767a255a49c6c433a0c302b
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2024-03-08 00:39:14 +0530

    css fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import SideBar from "./components/SideBar";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <SideBar />
-      <Header />
-      <Footer />
+      <div className="app-container">
+        <SideBar />
+        <div className="content">
+          <Header />
+          <Footer />
+        </div>
+      </div>
     </>
   );
 }
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,7 @@ const SideBar = () => {
   return (
     <>
       <div
-        className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark"
+        className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar"
         style={{ width: "280px" }}
       >
         <a
