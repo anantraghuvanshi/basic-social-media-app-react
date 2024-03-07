06243dc9f89543dedcfefd9213775f1ff7832665
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2024-03-07 23:29:03 +0530

    basic header, footerrs and sidebar add

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SideBar from "./components/SideBar";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <SideBar />
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
