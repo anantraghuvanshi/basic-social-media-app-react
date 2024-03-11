29a40d48dea0133ccf447aaa01f6163554ff1bb6
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2024-03-11 23:22:35 +0530

    posts configured

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,10 +3,16 @@ const Post = ({ post }) => {
     <>
       <div class="card post-card" style={{ width: "18rem" }}>
         <div class="card-body">
-          <h5 class="card-title">{post.title}</h5>
+          <h5 class="card-title">
+            {post.title}
+            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bag-danger">
+              {post.reactions}
+            </span>
+          </h5>
           <p class="card-text">{post.body}</p>
-          {post.tags.map({tag} => (<span className="badge text-bg-primary">{tag}</span>))}
-          
+          {post.tags.map((tag) => (
+            <span className="badge text-bg-primary hashtag">{tag}</span>
+          ))}
         </div>
       </div>
     </>
